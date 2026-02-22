8136dbd09bfe638d6e086bb12011db266b2a4931
nia <nia@NetBSD.org>
2026-02-22 15:14:47 +0100

    don't use C defines in JS

--- src/mudclient.c
+++ src/mudclient.c
@@ -9,8 +9,8 @@ EM_JS(int, can_resize, (), {
                document.activeElement !== window._mudclientPassword;
 });
 
-EM_JS(int, get_window_width, (), { return /*window.innerWidth*/MUD_VANILLA_WIDTH; });
-EM_JS(int, get_window_height, (), { return /*window.innerHeight*/MUD_VANILLA_HEIGHT; });
+EM_JS(int, get_window_width, (), { return /*window.innerWidth*/512; });
+EM_JS(int, get_window_height, (), { return /*window.innerHeight*/346; });
 
 EM_JS(void, browser_trigger_keyboard,
       (char *text, int is_password, int x, int y, int width, int height,
